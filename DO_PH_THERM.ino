351499b15ab1f268b76cc845fba67b6e6e1bf581
emi28 <38166576+emi28@users.noreply.github.com>
2019-01-05 09:10:21 -0800

    Add files via upload

---
+++ DO_PH_THERM.ino
@@ -0,0 +1,243 @@
+//=============
+//DO Code
+//=============
+
+#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
+#define rx 2                                          //define what pin rx is going to be
+#define tx 3                                          //define what pin tx is going to be
+
+//=============
+//PH Code
+//=============
+
+#define SensorPin A1 //pH meter Analog output to Arduino Analog Input 0
+#define Offset 0.00 //deviation compensate
+#define LED 13
+#define samplingInterval 20
+#define printInterval 975
+#define ArrayLenth 40 //times of collection
+
+//=============
+//Thermister
+//=============
+
+// which analog pin to connect
+#define THERMISTORPIN A0
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 10000
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 10000
+
+//=============
+//DO Code
+//=============
+
+SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+
+
+String inputstring = "";                              //a string to hold incoming data from the PC
+String sensorstring = "";                             //a string to hold the data from the Atlas Scientific product
+boolean input_string_complete = false;                //have we received all the data from the PC
+boolean sensor_string_complete = false;               //have we received all the data from the Atlas Scientific product
+float DO;                                             //used to hold a floating point number that is the DO
+
+//=============
+//PH Code
+//=============
+
+int pHArray[ArrayLenth]; //Store the average value of the sensor feedback
+int pHArrayIndex=0;
+
+//=============
+//Thermister
+//=============
+
+uint16_t samples[NUMSAMPLES];
+
+void setup()
+{
+//=============
+//PH Code
+//=============
+  pinMode(LED,OUTPUT);
+  Serial.begin(9600);
+  Serial.println("pH meter experiment!"); //Test the serial monitor
+
+//=============
+//DO Code
+//=============
+
+ Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
+  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
+  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
+  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
+
+//=============
+//Thermister
+//=============
+
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+  
+}
+
+ void serialEvent() {                                  //if the hardware serial port_0 receives a char
+  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
+  input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC 
+}
+
+void loop () {
+  DOcode();
+  PHcode();
+  THcode();
+}
+
+void DOcode(){                                         //here we go...
+
+  if (input_string_complete){                         //if a string from the PC has been received in its entirety
+    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
+    myserial.print('\r');                             //add a <CR> to the end of the string 
+    inputstring = "";                                 //clear the string
+    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+  }
+
+  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
+    char inchar = (char)myserial.read();              //get the char we just received
+    sensorstring += inchar;                           //add the char to the var called sensorstring
+    if (inchar == '\r') {                             //if the incoming character is a <CR>
+      sensor_string_complete = true;                  //set the flag
+    }
+  }
+
+
+  if (sensor_string_complete== true) {                //if a string from the Atlas Scientific product has been received in its entirety
+    Serial.println(sensorstring);                     //send that string to the PC's serial monitor
+    /*                                                //uncomment this section to see how to convert the DO reading from a string to a float 
+    if (isdigit(sensorstring[0])) {                   //if the first character in the string is a digit
+      DO = sensorstring.toFloat();                    //convert the string to a floating point number so it can be evaluated by the Arduino
+      if (DO >= 6.0) {                                //if the DO is greater than or equal to 6.0
+        Serial.println("high");                       //print "high" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
+      }
+      if (DO <= 5.99) {                               //if the DO is less than or equal to 5.99
+        Serial.println("low");                        //print "low" this is demonstrating that the Arduino is evaluating the DO as a number and not as a string
+      }
+    }
+    */
+    sensorstring = "";                                //clear the string
+    sensor_string_complete = false;                   //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  }
+}
+
+//===============
+
+void PHcode(){
+static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue,voltage;
+  if(millis()-samplingTime > samplingInterval)
+  {
+    pHArray[pHArrayIndex++]=analogRead(SensorPin);
+    if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+    voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+    pHValue = 3.5*voltage+Offset;
+    samplingTime=millis();
+}
+if(millis() - printTime > printInterval) //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+{
+    Serial.print("Voltage:");
+    Serial.print(voltage,2);
+    Serial.print(" pH value: ");
+    Serial.println(pHValue,2);
+    digitalWrite(LED,digitalRead(LED)^1);
+    printTime=millis();
+  }
+}
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount=0;
+  if(number<=0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number<5){ //less than 5, calculated directly statistics
+    for(i=0;i<number;i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0]<arr[1]){
+      min = arr[0];max=arr[1];
+  }
+  else{
+    min=arr[1];max=arr[0];
+  }
+  for(i=2;i<number;i++){
+    if(arr[i]<min){
+      amount+=min; //arr<min
+      min=arr[i];
+  }else {
+    if(arr[i]>max){
+      amount+=max; //arr>max
+      max=arr[i];
+  }else{
+      amount+=arr[i]; //min<=arr<=max
+    }
+  }//if
+}//for
+avg = (double)amount/(number-2);
+}//if
+return avg;
+}
+
+//============
+
+void THcode(){
+uint8_t i;
+  float average;
+  
+  // take N samples in a row, with a slight delay
+  for (i=0; i< NUMSAMPLES; i++) {
+    samples[i] = analogRead(THERMISTORPIN);
+    delay(10);
+}
+
+  // average all the samples out
+  average = 0;
+  for (i=0; i< NUMSAMPLES; i++) {
+    average += samples[i];
+}
+  average /= NUMSAMPLES;
+  
+  Serial.print("Average analog reading ");
+  Serial.println(average);
+  
+  // convert the value to resistance
+  average = 1023 / average - 1;
+  average = SERIESRESISTOR / average;
+  Serial.print("Thermistor resistance ");
+  Serial.println(average);
+
+  float steinhart;
+  steinhart = average / THERMISTORNOMINAL; // (R/Ro)
+  steinhart = log(steinhart); // ln(R/Ro)
+  steinhart /= BCOEFFICIENT; // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart; // Invert
+  steinhart -= 273.15; // convert to C
+
+  Serial.print("Temperature ");
+  Serial.print(steinhart);
+  Serial.println(" *C");
+
+  delay(1000);
+}
